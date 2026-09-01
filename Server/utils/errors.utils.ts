b700ce4b4e5168f2973d9c157b3a5a93fdeeef66
unknown <antoinecarrette@hotmail.fr>
2026-09-01 10:27:51 +0200

    build: server errors utils 01/09/2026

---
+++ Server/utils/errors.utils.ts
@@ -0,0 +1,41 @@
+export const signUpErrors = (err:any) => {
+    let errors = {name: "", email:"", password:"", description:""}
+
+    if (err.message.includes("name"))
+    errors.name = "Nom incorrect"
+
+    if (err.message.includes("email"))
+    errors.email = "Email incorrect"
+
+    if (err.message.includes("password"))
+    errors.password = "Mot de passe incorrect"
+
+    if(err.message.includes("description"))
+    errors.description = "Description incorrecte"
+
+    if (err.code === 11000 && Object.keys(err.keyValue)[0].includes("name"))
+        errors.name = "Ce nom est déjà pris"
+
+    if (err.code === 11000 && Object.keys(err.keyValue)[0].includes("email"))
+        errors.email = "Cette adresse mail est déjà prise"
+    
+    return errors
+}
+
+export const signInErrors = (err:any) => {
+    let errors = {email:"", password: ""}
+
+    if(err.message.includes("email"))
+        errors.email = "Email inconnu"
+    
+    if(err.message.includes("password"))
+        errors.password = "Mot de passe inconnu"
+}
+
+export const uploadErrors = (err:any) => {
+    let errors = {format:"", maxSize:""}
+
+    if (err.message.includes("invalid file"))
+    errors.format = "Format de fichier invalide"
+
+} 
