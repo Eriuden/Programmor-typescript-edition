8c43f2d184ffa97615110c69296442ef7608230c
unknown <antoinecarrette@hotmail.fr>
2026-09-01 10:32:09 +0200

    build: user controller 01/09/2026

---
+++ Server/controllers/uploadController.ts
@@ -0,0 +1,75 @@
+import { Request, Response } from "express";
+import { User } from "../models/User.model";
+import fs from "fs";
+import { promisify } from "util";
+import stream from "stream";
+import { uploadErrors } from "../utils/errors.utils";
+
+const pipeline = promisify(stream.pipeline);
+
+interface MulterFile {
+  detectedMimeType?: string;
+  size: number;
+  stream: NodeJS.ReadableStream;
+}
+
+interface CustomRequest extends Request {
+  file?: MulterFile;
+}
+
+const validateFile = (file?: MulterFile) => {
+  if (!file) throw new Error("Fichier manquant");
+
+  if (
+    file.detectedMimeType !== "image/jpg" &&
+    file.detectedMimeType !== "image/png" &&
+    file.detectedMimeType !== "image/jpeg"
+  ) {
+    throw new Error("Fichier invalide");
+  }
+
+  if (file.size > 500000) {
+    throw new Error("Taille maximale dépassée");
+  }
+};
+
+export const uploadProfil = async (
+  req: CustomRequest,
+  res: Response
+) => {
+  try {
+    validateFile(req.file);
+  } catch (error) {
+    const errors = uploadErrors(error);
+    return res.status(400).json({ errors });
+  }
+
+  const fileName = `${req.body.name}_${Date.now()}.jpg`;
+
+  try {
+    await pipeline(
+      req.file!.stream,
+      fs.createWriteStream(
+        `${__dirname}/../client/public/uploads/userImages/${fileName}`
+      )
+    );
+
+    const updatedUser = await User.findByIdAndUpdate(
+      req.body.userId,
+      {
+        $set: {
+          picture: `./uploads/userImages/${fileName}`,
+        },
+      },
+      { new: true }
+    );
+
+    return res.send(updatedUser);
+  } catch (error) {
+    return res.status(500).json({ error });
+  }
+};
+
+
+
+ 
