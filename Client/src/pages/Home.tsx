34bb12839f7c688e4785fbf1cc4845e73d51646a
unknown <antoinecarrette@hotmail.fr>
2026-09-06 14:18:08 +0200

    build: Home page 06/09/2026

---
+++ Client/src/pages/Home.tsx
@@ -0,0 +1,43 @@
+import {useState, useEffect} from "react"
+import { useDispatch, useSelector } from "react-redux"
+import { getUsers } from "../redux/actions/Users.action" 
+import UserCard from "../Components/UserCard"
+import { isEmpty } from "../utils"
+
+export const Home = () => {
+    const [loadCard, setLoadCard] = useState(false)
+    const [count, setCount] = useState(0)
+    const dispatch = useDispatch()
+    const users = useSelector((state:any)=> state.allUsersReducer)
+  
+  
+    const loadMore = () => {
+      if (window.innerHeight + document.documentElement.scrollTop + 1 >
+        document.scrollingElement!.scrollHeight)
+        {
+          setLoadCard(true)
+        }
+    }
+  
+    useEffect(()=> {
+      if (loadCard) {
+        getUsers(count, dispatch)
+        setLoadCard(false)
+        setCount(count + 10)
+      }
+      window.addEventListener("scroll", loadMore)
+    }, [loadCard, dispatch, count])
+    
+    return (
+      <div>
+        <div>
+          <ul>
+            {!isEmpty(users[0]) &&
+              users.map((user:any) => {
+                return <UserCard userProps={user} key={user._id}/>
+            })}
+          </ul>
+        </div>
+      </div>
+    )
+}
